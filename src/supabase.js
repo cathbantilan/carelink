67ccde4634fcd2bb7406efa0bb66c66e3fa4d0ad
cat <cathbantilan@gmail.com>
2025-01-07 11:56:43 -0800

    added supa into env

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lrjnqrymzpqdnikglplb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyam5xcnltenBxZG5pa2dscGxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0NzUxOTYsImV4cCI6MjA1MTA1MTE5Nn0.MftD2d4ShqdoGbv5YmXyFGDaR9OiryO7i9ZzEgIEtUU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
